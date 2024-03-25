f721a8fa474144e168ed2d829d777e5d2409afbb
Nelson Vides <videsnelson@gmail.com>
2024-03-25 11:19:13 +0100

    Avoid autocomplete on enter automatically, first choose

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -389,14 +389,17 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 }),
                 sources = cmp.config.sources(
                     {
                         { name = 'nvim_lsp' },
                         { name = 'neorg' },
                         { name = 'luasnip' }, -- For luasnip users.
-                        { name = 'path', option = { trailing_slash = true } },
+                        { name = 'path', option = {
+                            trailing_slash = true,
+                            label_trailing_slash = true
+                        } },
                         { name = 'calc' },
                         { name = 'nvim_lsp_signature_help' },
                         { name = 'cmp_yanky' }
@@ -425,7 +428,10 @@ return {
             cmp.setup.cmdline(':', {
                 mapping = cmp.mapping.preset.cmdline(),
                 sources = cmp.config.sources({
-                    { name = 'path' }
+                    { name = 'path', option = {
+                        trailing_slash = true,
+                        label_trailing_slash = true
+                    } },
                 }, {
                     { name = 'cmdline' }
                 })
