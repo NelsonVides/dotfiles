da57d3da7e88637dd19aca006e4288921ea0ca75
Nelson Vides <videsnelson@gmail.com>
2024-04-13 15:18:49 +0200

    Enable vim.lsp update_in_insert

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -99,3 +99,10 @@ nvim_lsp.ltex.setup({
     },
     on_attach = on_attach,
 })
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- delay update diagnostics
+    update_in_insert = true,
+  }
+)
