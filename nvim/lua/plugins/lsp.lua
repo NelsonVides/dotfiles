a072291fd8ee7ca3405c7479338b798c99a355de
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-04-18 13:27:20 +0200

    Small shuffle in the lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -41,13 +41,6 @@ end
 --   }
 -- }
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        -- delay update diagnostics
-        update_in_insert = true,
-    }
-)
-
 return {
     {
         'neovim/nvim-lspconfig',
@@ -59,6 +52,13 @@ return {
             local nvim_lsp = require('lspconfig')
             local client_capabilities = vim.lsp.protocol.make_client_capabilities()
             local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+                vim.lsp.diagnostic.on_publish_diagnostics, {
+                    -- delay update diagnostics
+                    update_in_insert = true,
+                }
+            )
 
             local path_to_elp = vim.fn.expand("~/repos/public/erlang-language-platform/releases/elp")
             nvim_lsp.elp.setup({
