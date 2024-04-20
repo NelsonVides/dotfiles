4531bb01df2f3fe1492ad6178c92c23849461382
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-04-20 14:48:30 +0200

    Install jsregexp with LuaSnip

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
     { 'rafamadriz/friendly-snippets' },
     { 'honza/vim-snippets' },
     { 'L3MON4D3/LuaSnip',
+        build = "make install_jsregexp",
         dependencies = { 'rafamadriz/friendly-snippets' },
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
