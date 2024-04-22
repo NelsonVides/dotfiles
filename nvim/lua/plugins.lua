505ad423680b086436df400f80f2b2bbeb8022dc
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-04-22 13:36:10 +0200

    Make branch take less space in nvim-lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -11,7 +11,7 @@ return {
                 },
                 sections = {
                     lualine_a = {{'mode', fmt = function(str) return str:sub(1,1) end}},
-                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_b = {{'branch', fmt = function(str) return str:sub(1,1) end}, 'diff', 'diagnostics'},
                     lualine_c = {'filename'},
                     lualine_x = {'encoding', 'fileformat'},
                     lualine_y = {'filetype'},
