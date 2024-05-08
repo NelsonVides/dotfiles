862ec00f025a12dd29945b19ae0d3f1d4d2199de
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-05-08 09:35:12 +0200

    Improve snippets handling and add custom ones

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,10 +4,16 @@ return {
     { 'honza/vim-snippets' },
     { 'L3MON4D3/LuaSnip',
         build = "make install_jsregexp",
-        dependencies = { 'rafamadriz/friendly-snippets' },
+        dependencies = {
+            'rafamadriz/friendly-snippets',
+            'honza/vim-snippets'
+        },
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
             require("luasnip.loaders.from_snipmate").lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load({
+                paths = { vim.fn.stdpath("config") .. "/snippets" },
+            })
         end,
         keys = {
             {
