754ab89abc6801f2c9b5c9b738984163d689a4b0
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-05-17 12:50:00 +0200

    Add nvim alternate-toggler plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -145,4 +145,20 @@ return {
 
     { 'andymass/vim-matchup' },
 
+    { 'rmagatti/alternate-toggler',
+        event = { "BufReadPost" }, -- lazy load after reading a buffer
+        config = function()
+            require("alternate-toggler").setup {
+                alternates = {
+                    ["ok"] = "error",
+                    ["left"] = "right",
+                    ["=="] = "!=",
+                }
+            }
+            vim.keymap.set("n", "<leader><space>", -- <space><space>
+                "<cmd>lua require('alternate-toggler').toggleAlternate()<CR>"
+            )
+        end,
+    }
+
 }
