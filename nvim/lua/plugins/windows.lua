ec440fd6e02031c9a71bd988aeb33e45e2c98cf0
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-12-20 09:32:36 +0100

    Reconfigue NeoZoom

--- nvim/lua/plugins/windows.lua
+++ nvim/lua/plugins/windows.lua
@@ -115,15 +115,11 @@ return {
     { 'nyngwang/NeoZoom.lua',
         config = function ()
             require('neo-zoom').setup {
-                popup = { enabled = true }, -- this is the default.
-                -- NOTE: Add popup-effect (replace the window on-zoom with a `[No Name]`).
-                -- EXPLAIN: This improves the performance, and you won't see two
-                --          identical buffers got updated at the same time.
-                -- popup = {
-                --   enabled = true,
-                --   exclude_filetypes = {},
-                --   exclude_buftypes = {},
-                -- },
+                popup = {
+                  enabled = true,
+                  exclude_filetypes = {},
+                  exclude_buftypes = {},
+                },
                 exclude_buftypes = { 'terminal' },
                 -- exclude_filetypes = { 'lspinfo', 'mason', 'lazy', 'fzf', 'qf' },
                 winopts = {
@@ -131,7 +127,7 @@ return {
                         -- NOTE: omit `top`/`left` to center the floating window vertically/horizontally.
                         -- top = 0,
                         -- left = 0.17,
-                        width = 150,
+                        width = 120,
                         height = 0.85,
                     },
                     -- NOTE: check :help nvim_open_win() for possible border values.
@@ -139,14 +135,7 @@ return {
                 },
                 presets = {
                     {
-                        -- NOTE: regex pattern can be used here!
-                        filetypes = { 'dapui_.*', 'dap-repl' },
-                        winopts = {
-                            offset = { top = 0.02, left = 0.26, width = 0.74, height = 0.25 },
-                        },
-                    },
-                    {
-                        filetypes = {"markdown", "norg"},
+                        filetypes = {"markdown"},
                         callbacks = {
                             function () vim.wo.wrap = true end,
                         },
