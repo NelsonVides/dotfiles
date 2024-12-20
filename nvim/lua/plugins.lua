543d5d650f69fc7f8ddfa4537695be7d42fe3ff8
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-12-20 09:24:08 +0100

    Replace undotree by telescope-undo

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,12 +9,22 @@ return {
         },
     },
 
-    { 'jiaoshijie/undotree', lazy = true,
-        dependencies = 'nvim-lua/plenary.nvim',
-        config = true,
-        keys = {
-            { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    { 'debugloop/telescope-undo.nvim',
+        dependencies = {
+            {
+                'nvim-telescope/telescope.nvim',
+                dependencies = { 'nvim-lua/plenary.nvim' },
+            },
         },
+        keys = { { "<leader>u", "<cmd>Telescope undo<cr>", desc = "undo history" } },
+        opts = { extensions = { undo = { } } },
+        config = function(_, opts)
+            -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+            -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+            -- defaults, as well as each extension).
+            require("telescope").setup(opts)
+            require("telescope").load_extension("undo")
+        end,
     },
 
     { 'olimorris/persisted.nvim', lazy = false,
